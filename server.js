720ac42348988f45715e3cf7c9192e7a1ecc0298
Ravi Prakash <ravimehra1264@gmail.com>
2020-05-24 17:38:04 +0530

    Server.js file updated

--- server.js
+++ server.js
@@ -95,7 +95,7 @@ app.post('/teachers/addque',(request,response)=>{
     client.connect()
     .then(()=>{
         console.log('Connection Complete');
-        const sql= 'INSERT INTO  addque(qu1,que2,que3,que4,que5) VALUES ($1, $2, $3, $4, $5)'
+        const sql= 'INSERT INTO  addque(que1,que2,que3,que4,que5) VALUES ($1, $2, $3, $4, $5)'
         const params = [request.body.que1,request.body.que2,request.body.que3,request.body.que4,request.body.que5];
         return client.query(sql,params);
     })
